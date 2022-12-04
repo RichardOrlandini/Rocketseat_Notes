fac440e18f56c72cfff84d4cb72d6493989d3cba
Richard Orlandini de Souza Aleixo <t02939@complexo.unimed>
2022-12-04 20:01:07 -0300

    r

---
+++ src/components/Note/index.jsx
@@ -0,0 +1,23 @@
+import { Container } from './styles';
+import { Tag} from '../../components/Tag'
+
+export function Note ({data, ...rest}){
+    return (
+        <Container {...rest} >
+            <h1>{data.title}</h1>
+        {
+            data.tags && 
+            <footer>
+                {
+                    data.tags.map(tag => {
+                        <Tag 
+                        key={tag.name}
+                        title={tag.name} />
+                    })
+                }
+            </footer>
+
+        }
+        </Container>
+    )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,12 @@
+import { FiPlus, FiSearch } from 'react-icons/fi';
+
 import {Container, Brand, Menu, Search, Content, NewNote} from './styles';
 
+import { Note } from '../../components/Note';
 import { Header } from '../../components/Header'
+import { Input } from '../../components/Input';
+import { Section } from '../../components/Section'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Home(){
     return (
@@ -14,19 +20,33 @@ export function Home(){
       
 
           <Menu>
-
+            <li><ButtonText title="Todos" isActive/></li>
+            <li><ButtonText title="Todos" /></li>
+            <li><ButtonText title="Todos" /></li>
           </Menu>
 
           <Search>
-
+            <Input 
+            placeholder= "Pesquisar pelo título" 
+            icon={FiSearch} />
           </Search>
 
           <Content>
-
+            <Section title="Minhas notas">
+              <Note data={{ 
+                title: 'react' ,
+                 tags:[
+                  {id: '1', name:'react'},
+                  {id: '2', name:'rocktseat'},
+                  ]
+                }} 
+                />  
+            </Section>
           </Content>
 
           <NewNote>
-
+            <FiPlus/>
+              Criar nota
           </NewNote>
         </Container>
     );

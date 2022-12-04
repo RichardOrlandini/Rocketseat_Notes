663427c49f94dab89116dde31cbbe24705d17d7e
Richard Orlandini de Souza Aleixo <t02939@complexo.unimed>
2022-12-04 16:58:16 -0300

    Aula de children

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,21 @@
-import { Container } from "./styles";
+import { Container , Links} from "./styles";
+
 import { Header }  from "../../components/Header"
 import { Button } from "../../components/Button";
-
+import { Section } from "../../components/Section";
 export function Details(){
 
   return (
     <Container>  
       <Header/>
-    <Button title=" Voltar "  />
+    <Section title="Links úteis">
+      <Links>
+        <li><a href="#">Link 1</a></li>
+        <li><a href="#">Link 1</a></li>
+      </Links>
+    </Section>
 
+    <Button title=" Voltar "  />
     </Container>
 
   )

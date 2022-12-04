f3bf10efe3335dd19127f6931ef1673f8403077a
Richard Orlandini <richardorlandini09@gmail.com>
2022-12-04 16:38:27 -0300

    Subindo o repositorio

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,23 @@
+
+import {RiShutDownLine} from 'react-icons/ri';
+import { Container, Profile, Logout } from "./styled";
+
+export function Header(){
+    return (
+        <Container>
+            <Profile>
+                <img src="https://github.com/RichardOrlandini" 
+                alt="Foto do usuário" />
+
+                <div>
+                    <span>Bem vindo</span>
+                    <strong>Richard Orlandini</strong>
+                </div>
+            </Profile>
+
+            <Logout>
+                <RiShutDownLine/>
+            </Logout>
+        </Container>
+    );
+}
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,15 @@
+import { Container } from "./styles";
+import { Header }  from "../../components/Header"
+import { Button } from "../../components/Button";
+
+export function Details(){
+
+  return (
+    <Container>  
+      <Header/>
+    <Button title=" Voltar "  />
+
+    </Container>
+
+  )
+}
